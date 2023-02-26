21aff39d69f32312d4a280452f45a8b646bbf4f0
franciska98 <franciska.miletic8@gmail.com>
2023-02-26 19:51:18 +0100

    Finish app

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (binding.textView.visibility == View.VISIBLE) hide()
+            else show()
+        }
+    }
+
+    private fun show() {
+        binding.button.setText(R.string.hide)
+        binding.textView.visibility = View.VISIBLE
+    }
+
+    private fun hide() {
+        binding.button.setText(R.string.show)
+        binding.textView.visibility = View.INVISIBLE
     }
 }
